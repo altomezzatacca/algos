24bd9f1804b114a7b47277314b871947de03015a
altomezzatacca <taccagianmario@gmail.com>
2026-09-10 11:56:29 +0200

    .

---
+++ src/template.cpp
@@ -0,0 +1,35 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define endl "\n"
+#define __ << " " <<
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+template<typename... Args>
+using vec = vector<Args...>;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+constexpr static int inf = 2e9;
+constexpr static ll linf = 1e18;
+constexpr static int mod1 = 1e9 + 7;
+constexpr static int mod2 = 998244353;
+
+// template per codice c++ particolarmente lungo
+// compilare con: g++ codice.cpp -o a -O1 -g -fsanitize=address,undefined -Wall -Wextra -Wshadow -Wpedantic
+
+void solve() {
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    int t; cin >> t; // testcase
+    while (t--)
+        solve();
+}
